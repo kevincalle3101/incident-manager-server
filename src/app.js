e57d389ca15afe8a629f201be099114789791b28
kevincalle3101 <kevinmendoza3101@hotmail.com>
2024-02-11 18:04:24 -0500

    other change url

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ server.use(morgan("dev"));
 server.use(bodyParser.json());
 server.use(
   cors({
-    origin: "https://incident-manager-client.vercel.app/",
+    origin: "*",
     methods: "GET,PUT,POST,DELETE",
     credentials: true,
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
   })
 );
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://incident-manager-client.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
